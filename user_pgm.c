d4786667a183d49ecba3af708279ec9240fd9efc
msilver8 <msilver8@terpmail.umd.edu>
2019-11-13 17:03:09 -0500

    More user friendly

--- user_pgm.c
+++ user_pgm.c
@@ -18,14 +18,28 @@ void print_char(char to_send) {
   exit(0);
 }
 
+/* 
+ * This function should contain all of your code. It gets the strings state and guesses
+ * and the int num_guesses as parameters. state is a string representing the current
+ * state of the secret word. Any characters in the string that have not been correctly
+ * guessed yet are represented as '_'. Example: if the word to be guessed is "apple",
+ * and you have previously guessed 'a' and 'l', the string will be "a__l_" (ending with
+ * a null terminating byte). guesses is a character array of the previously guessed
+ * characters ending in a null terminating byte. The return value should be the
+ * character that you want to guess.
+ */
+char user_func(char* state, char* guesses, int num_guesses) {
+
+  /* YOUR CODE HERE */
+  
+}
+
 int main(void) {
   char *state, *guesses, to_send;
   int num_guesses;
 
   get_in(state, guesses, &num_guesses);
 
-  /* YOUR CODE HERE */
-
-  print_char(to_send);
+  print_char(user_func(state, guesses, num_guesses));
   return 0;
 }
